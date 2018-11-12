59e6ae02bd4ba326603ea6a49fde79908e9c4d4f
peerakit umbangtalad <>
2018-11-12 14:12:59 +0700

    no message

--- ar.php
+++ ar.php
@@ -63,6 +63,9 @@ function storeUserImage($id,$imgId,$header){
     //$ch = "curl -v -X "." GET ".$strUrl." -o http://sheltered-refuge-45467.herokuapp.com/meme/userImg/".$imgId.".png "." -H '"."$header'";
     $ch = "curl -v -X "." GET "."https://api.line.me/v2/bot/message/8851169895483/content"." -o meme/userImg/"."8851169895483".".png "." -H '"."Authorization: Bearer uUE/X13a2XpVT0CAFsl+x3PTTxcFwHvYsrF2Mg8Vt5LAwEI8/v6To55m+cDqoj8iKTYQ9QHndnGYHRuB3ZXwGSwsAmoKcNzS1nWx1vGZ3vPp3KNwi0eWuxSz4AfkuH0fP2wUt5pwgfZsCKZRJp52CgdB04t89/1O/w1cDnyilFU='";
     exec($ch,$output,$code);
+    $url = 'http://sheltered-refuge-45467.herokuapp.com/meme/userImg/8851169895483.png';
+    $img = '/workspace/App in line/AngelingBot/meme/userImg/8851169895483.png';
+    file_put_contents($img, file_get_contents($url));
     //$path = "/meme/userImg/$imgId.png";
     //file_put_contents($path,base64_decode($output));
     return $code;
