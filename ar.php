7ac7c3d3fc8544242de95fd832e232af3d852cfc
peerakit umbangtalad <>
2018-11-12 12:01:08 +0700

    no message

--- ar.php
+++ ar.php
@@ -8,7 +8,7 @@ function arManagement($conn,$id,$state,$message){
         storeUserImage($id,$message,$header);
         $sql = "UPDATE open_session SET state = 1 WHERE u_id = '$id' AND status = '1'";
         $conn->query($sql);
-        return __DIR__;
+        return "กรุณาเลือกแท็กที่ต้องการใส่โดยพิมพ์เลขด้านหน้าแท็กครับ\n1 #ประเทศกูมี\n2 #RapThailand4.0\n3 #คุกกี้เสี่ยงทาย\n4 #คุกกี้เสี่ยงคุก";
     }
     else if($state == 1){
         if($message == "1"){
