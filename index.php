7e8582c61c848ed2f4fb05a0e3fd0ea7cd9e6c21
peerakit umbangtalad <>
2018-11-12 16:14:54 +0700

    no message

--- index.php
+++ index.php
@@ -150,8 +150,8 @@ imagedestroy($im2);
 
 
 
-$backgroundImage=__DIR__."./meme/userImg/background.png";
-$overlayImage=__DIR__.'./meme/template/test.png'; 
+$backgroundImage=__DIR__."meme/userImg/background.png";
+//$overlayImage=__DIR__.'./meme/template/test.png'; 
 
 $im = imagecreatefrompng($backgroundImage);
 //$im2 = imagecreatefrompng($overlayImage);
